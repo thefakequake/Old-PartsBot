ad3a0b7bbb9c64539c38b6071e9ec6e9ebf5f6c2
QuaKe8782 <mastermind4560@gmail.com>
2021-01-13 22:38:16 +0000

    Remove unlock command

--- main.py
+++ main.py
@@ -14,8 +14,10 @@ import aiosqlite
 
 with open("credentials.json") as file:
     data = json.load(file)
+with open("countries.json") as file:
+    country_data = json.load(file)
 
-red = discord.Colour.from_rgb(0, 100, 0)
+green = discord.Colour(0x1e807c)
 error_colour = discord.Colour.from_rgb(254, 0, 0)
 yellow = discord.Colour.from_rgb(254, 254, 0)
 
@@ -23,7 +25,21 @@ intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(help_command=None, command_prefix=commands.when_mentioned_or(data["command_prefix"]), intents=intents, case_insensitive=True)
 
+bot.reactions = ["1\N{variation selector-16}\N{combining enclosing keycap}",
+                 "2\N{variation selector-16}\N{combining enclosing keycap}",
+                 "3\N{variation selector-16}\N{combining enclosing keycap}",
+                 "4\N{variation selector-16}\N{combining enclosing keycap}",
+                 "5\N{variation selector-16}\N{combining enclosing keycap}",
+                 "6\N{variation selector-16}\N{combining enclosing keycap}",
+                 "7\N{variation selector-16}\N{combining enclosing keycap}",
+                 "8\N{variation selector-16}\N{combining enclosing keycap}",
+                 "9\N{variation selector-16}\N{combining enclosing keycap}",
+                 "\N{keycap ten}",
+                 "\u274C"]
 
+bot.countries = country_data
+
+bot.botadmins = [287256464047865857, 405798011172814868]
 
 @bot.event
 async def on_ready():
@@ -38,7 +54,7 @@ async def on_ready():
             print(f"cogs.{name} loaded")
     print("PartsBot is ready.")
     channel = bot.get_channel(769906608318316594)
-    embed_msg = discord.Embed(title="Bot restarted.", colour=red, timestamp=datetime.utcnow())
+    embed_msg = discord.Embed(title="Bot restarted.", colour=green, timestamp=datetime.utcnow())
     await channel.send(embed=embed_msg)
     while True:
         await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=",help"))
@@ -54,120 +70,122 @@ async def on_ready():
 
 @bot.command()
 async def load(ctx, cog):
-    if ctx.author.id in (287256464047865857, 405798011172814868):
+    if ctx.author.id in bot.botadmins:
         if cog.lower() == 'all':
             for filename in os.listdir("cogs"):
                 if filename.endswith(".py"):
                     name = filename.replace(".py", "")
                     bot.load_extension(f"cogs.{name}")
             embed_msg = discord.Embed(title=f"Sucessfully loaded all cogs.",
-                                      colour=red,
+                                      colour=green,
                                       timestamp=datetime.utcnow())
             await ctx.send(embed=embed_msg)
         else:
             try:
                 bot.load_extension(f"cogs.{cog}")
                 embed_msg = discord.Embed(title=f"Sucessfully loaded the cog '{cog}'.",
-                                          colour=red,
+                                          colour=green,
                                           timestamp=datetime.utcnow())
                 await ctx.send(embed=embed_msg)
             except discord.ext.commands.ExtensionNotFound:
                 embed_msg = discord.Embed(title=f"Cog '{cog}' not found.",
-                                          colour=red,
+                                          colour=green,
                                           timestamp=datetime.utcnow())
                 await ctx.send(embed=embed_msg)
             except discord.ext.commands.ExtensionAlreadyLoaded:
                 embed_msg = discord.Embed(title=f"Cog is already loaded!",
-                                          colour=red,
+                                          colour=green,
                                           timestamp=datetime.utcnow())
                 await ctx.send(embed=embed_msg)
 
     else:
         embed_msg = discord.Embed(title="You don't have permission to use this command!",
-                                  colour=red,
+                                  colour=green,
                                   timestamp=datetime.utcnow())
         await ctx.send(embed=embed_msg)
 
 @bot.command(aliases=['re'])
 async def reload(ctx, cog):
-    if ctx.author.id in (287256464047865857, 405798011172814868):
+    if ctx.author.id in bot.botadmins:
         if cog.lower() == 'all':
             for filename in os.listdir("cogs"):
                 if filename.endswith(".py"):
                     name = filename.replace(".py", "")
                     bot.reload_extension(f"cogs.{name}")
             embed_msg = discord.Embed(title=f"Sucessfully reloaded all cogs.",
-                                      colour=red,
+                                      colour=green,
                                       timestamp=datetime.utcnow())
             await ctx.send(embed=embed_msg)
         else:
             try:
                 bot.reload_extension(f"cogs.{cog}")
                 embed_msg = discord.Embed(title=f"Sucessfully reloaded the cog '{cog}'.",
-                                          colour=red,
+                                          colour=green,
                                           timestamp=datetime.utcnow())
                 await ctx.send(embed=embed_msg)
             except discord.ext.commands.ExtensionNotFound:
                 embed_msg = discord.Embed(title=f"Cog '{cog}' not found.",
-                                          colour=red,
+                                          colour=green,
                                           timestamp=datetime.utcnow())
                 await ctx.send(embed=embed_msg)
             except discord.ext.commands.ExtensionNotLoaded:
                 embed_msg = discord.Embed(title=f"Cog is not loaded!",
-                                          colour=red,
+                                          colour=green,
                                           timestamp=datetime.utcnow())
                 await ctx.send(embed=embed_msg)
 
     else:
         embed_msg = discord.Embed(title="You don't have permission to use this command!",
-                                  colour=red,
+                                  colour=green,
                                   timestamp=datetime.utcnow())
         await ctx.send(embed=embed_msg)
 
+
+
 @bot.command(aliases=['un'])
 async def unload(ctx, cog):
-    if ctx.author.id in (287256464047865857, 405798011172814868):
+    if ctx.author.id in bot.botadmins:
         if cog.lower() == 'all':
             for filename in os.listdir("cogs"):
                 if filename.endswith(".py"):
                     name = filename.replace(".py", "")
                     bot.unload_extension(f"cogs.{name}")
             embed_msg = discord.Embed(title=f"Sucessfully unloaded all cogs.",
-                                      colour=red,
+                                      colour=green,
                                       timestamp=datetime.utcnow())
             await ctx.send(embed=embed_msg)
         else:
             try:
                 bot.unload_extension(f"cogs.{cog}")
                 embed_msg = discord.Embed(title=f"Sucessfully unloaded the cog '{cog}'.",
-                                          colour=red,
+                                          colour=green,
                                           timestamp=datetime.utcnow())
                 await ctx.send(embed=embed_msg)
             except discord.ext.commands.ExtensionNotFound:
                 embed_msg = discord.Embed(title=f"Cog '{cog}' not found.",
-                                          colour=red,
+                                          colour=green,
                                           timestamp=datetime.utcnow())
                 await ctx.send(embed=embed_msg)
             except discord.ext.commands.ExtensionNotLoaded:
                 embed_msg = discord.Embed(title=f"Cog is already unloaded!",
-                                          colour=red,
+                                          colour=green,
                                           timestamp=datetime.utcnow())
                 await ctx.send(embed=embed_msg)
 
     else:
         embed_msg = discord.Embed(title="You don't have permission to use this command!",
-                                  colour=red,
+                                  colour=green,
                                   timestamp=datetime.utcnow())
         await ctx.send(embed=embed_msg)
 
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandOnCooldown):
-        embed_msg = discord.Embed(title=str(error), timestamp=datetime.utcnow(), colour=red)
+        embed_msg = discord.Embed(title=str(error), timestamp=datetime.utcnow(), colour=green)
         await ctx.send(embed=embed_msg)
         return
     if isinstance(error, commands.MissingRequiredArgument):
-        embed_msg = discord.Embed(title="Missing Required Argument!", description=f"You are seeing this error because the command you are trying to use needs more information to function.\nType `,help [name of command]` to see usage for that command.", timestamp=datetime.utcnow(), colour=red)
+        embed_msg = discord.Embed(title="Missing Required Argument!", description=f"You are seeing this error because the command you are trying to use needs more information to function.\nType `,help [name of command]` to see usage for that command.", timestamp=datetime.utcnow(), colour=green)
         await ctx.send(embed=embed_msg)
         return
     if isinstance(error, commands.CommandNotFound):
@@ -175,17 +193,18 @@ async def on_command_error(ctx, error):
         command = ctx.message.content[1:].split(" ")
         highest = process.extractOne(command[0], commandslist)
         if int(highest[1]) > 80:
-            embed_msg = discord.Embed(title=f"Command '{command[0]}' not found.", description=f'Perhaps you meant \'**{highest[0]}**\'.', timestamp=datetime.utcnow(), colour=red)
+            embed_msg = discord.Embed(title=f"Command '{command[0]}' not found.", description=f'Perhaps you meant \'**{highest[0]}**\'.', timestamp=datetime.utcnow(), colour=green)
             await ctx.send(embed=embed_msg)
         return
     if isinstance(error, commands.MissingPermissions):
-        embed_msg = discord.Embed(title="Missing Permissions!", description=str(error), timestamp=datetime.utcnow(), colour=red)
+        embed_msg = discord.Embed(title="Missing Permissions!", description=str(error), timestamp=datetime.utcnow(), colour=green)
         await ctx.send(embed=embed_msg)
         return
-    embed_msg = discord.Embed(title="Oops! Something went wrong...", description="Looks like I've encountered an error. I have sent a bug report to the [PartsBot Discord](https://discord.gg/WM9pHp8).")
+    embed_msg = discord.Embed(title="Oops! Something went wrong...", description="Looks like I've encountered an error. I have sent a bug report to the [PartsBot Discord](https://discord.gg/WM9pHp8).", colour=green)
     channel = bot.get_channel(773989689060229180)
     embed_msg = discord.Embed(title=f"Error: {str(error)}", description=f"**Text:**\n{ctx.message.content}\n\n**User ID:**\n{ctx.author.id}\n\n**Full Details:**\n{str(ctx.message)}", colour=error_colour, timestamp=datetime.utcnow())
     await channel.send(embed=embed_msg)
+    raise error
 
 
 
@@ -195,7 +214,7 @@ async def on_guild_join(guild):
     for channel in guild.channels:
         if worked is False:
             try:
-                embed_msg = discord.Embed(title='Thanks for adding PartsBot to your server!', colour=red,
+                embed_msg = discord.Embed(title='Thanks for adding PartsBot to your server!', colour=green,
                                           timestamp=datetime.utcnow())
 
                 embed_msg.add_field(name='Here\'s a few things you can try:',
@@ -206,6 +225,7 @@ async def on_guild_join(guild):
                 - Doing `,partprice [name of part]`
                 - Doing `,randompost`
                 If you need additional help, join the [Offical Discord](https://discord.gg/WM9pHp8) or contact **QuaKe#5943**.
+                Enjoying PartsBot? Consider supporting our work on [Patreon](https://www.patreon.com/partsbot).
                 ''')
                 await channel.send(embed=embed_msg)
                 worked = True
